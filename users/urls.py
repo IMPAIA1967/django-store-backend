75d4289d706f75868a25c3d07a12705b9c3a9eb0
Dmitry <dima.soin2014@yandex.ru>
2025-11-14 03:43:18 +0300

    добавлен и исправлен выход пользователя

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 from . import views
+from .views import email_verification
 
 app_name = 'users'
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path("email-confirm/<str:token>", views.email_verification, name="email_verification"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,18 @@
 from django.contrib.auth import login
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.core.mail import send_mail
 from django.conf import settings
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib import messages
+from django.http import Http404
+from .models import CustomUser, EmailVerificationToken
+import secrets
 
 from .forms import CustomUserCreationForm
+from .models import CustomUser
+
 
 class RegisterView(CreateView):
     form_class = CustomUserCreationForm
@@ -13,18 +20,30 @@ class RegisterView(CreateView):
     success_url = reverse_lazy('catalog:home')
 
     def form_valid(self, form):
-        user = form.save()
-        login(self.request, user)  # автоматический вход после регистрации
+        user = form.save(commit=False)  # Не сохраняем сразу
+        user.is_active = False  # Пользователь не активен до подтверждения
+        user.save()
 
-        # Отправка приветственного письма
+        # Создаём токен
+        token = secrets.token_hex(32)
+        EmailVerificationToken.objects.create(user=user, token=token)
+
+
+        # Отправляем письмо
+        host = self.request.get_host()
+        url = f'http://{host}{reverse("users:email_verification", kwargs={"token": token})}'
+
         send_mail(
-            subject='Добро пожаловать!',
-            message='Спасибо за регистрацию на нашем сайте!',
-            from_email=settings.DEFAULT_FROM_EMAIL,
+            subject="Подтверждение почты",
+            message=f"Привет! Перейди по ссылке для подтверждения почты:\n\n{url}",
+            from_email=settings.EMAIL_HOST_USER,
             recipient_list=[user.email],
             fail_silently=False,
         )
+
+        messages.success(self.request, "Письмо с подтверждением отправлено на ваш email.")
         return super().form_valid(form)
+
 
 class CustomLoginView(LoginView):
     template_name = 'users/login.html'
@@ -33,4 +52,18 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('catalog:home')
 
+
+def email_verification(request, token):
+    try:
+        verification_token = get_object_or_404(EmailVerificationToken, token=token)
+        user = verification_token.user
+        user.is_active = True
+        user.save()
+        verification_token.delete()  # Удаляем токен после использования
+
+        messages.success(request, "Email успешно подтверждён! Теперь вы можете войти.")
+        return redirect('users:login')
+    except Http404:
+        messages.error(request, "Ссылка подтверждения недействительна или устарела.")
+        return redirect('users:login')
 
