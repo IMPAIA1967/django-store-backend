1d4e6fc7d861f598d552f9e9b66794068a3c82d5
Dmitry <dima.soin2014@yandex.ru>
2025-11-18 19:13:15 +0300

    end

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .forms import ProductForm
 from .models import Product
 
@@ -17,6 +19,21 @@ class ProductDetailView(DetailView):
         obj.save(update_fields=['views'])
         return obj
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = self.get_object()
+
+        # Проверяем, является ли пользователь владельцем или модератором
+        is_owner = self.request.user == product.owner
+        is_moderator = (
+                self.request.user.is_authenticated and
+                self.request.user.groups.filter(name='Модератор продуктов').exists()
+        )
+
+        context['can_edit_or_delete'] = is_owner or is_moderator
+        context['can_unpublish'] = self.request.user.has_perm('catalog.can_unpublish_product')
+
+        return context
 
 class HomeView(ListView):
     model = Product
@@ -32,6 +49,10 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:home')
 
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
@@ -39,12 +60,32 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:home')
 
+    def get_queryset(self):
+        # Владелец или модератор могут редактировать
+        if self.request.user.groups.filter(name='Модератор продуктов').exists():
+            return Product.objects.all()
+        return Product.objects.filter(owner=self.request.user)
+
 
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:home')
 
+    def get_queryset(self):
+        # Владелец или модератор могут удалять
+        if self.request.user.groups.filter(name='Модератор продуктов').exists():
+            return Product.objects.all()
+        return Product.objects.filter(owner=self.request.user)
+
+class ProductUnpublishView(PermissionRequiredMixin, View):
+    permission_required = 'catalog.can_unpublish_product'
+
+    def post(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        product.is_published = False
+        product.save()
+        return HttpResponseRedirect(reverse_lazy('catalog:product_detail', kwargs={'pk': pk}))
 
 # def product_detail(request, pk):
 #     """Отображает страницу одного товара по его ID (pk)."""
